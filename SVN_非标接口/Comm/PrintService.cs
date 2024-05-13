09592dc5902e8ebff3d944adce0c72bd16e181a4
chenweijie <chenweijie@126.com>
2024-05-13 18:03:48 +0800

    a

---
+++ SVN_非标接口/Comm/PrintService.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HealthExaminationSystem.ThirdParty.DataTransmission.Comm
+{
+   public class PrintService
+    {
+        public PrintService()
+        {
+
+        }
+
+        #region Members //成员
+        public String printName = String.Empty;
+        public Font prtTextFont = new Font("Verdana", 16);
+        public Font prtTitleFont = new Font("宋体", 16);
+        private String[] titles = new String[0];
+        public String[] Titles
+        {
+            set
+            {
+                titles = value as String[];
+                if (null == titles)
+                {
+                    titles = new String[0];
+                }
+            }
+            get
+            {
+                return titles;
+            }
+        }
+        private Int32 left = 20;
+        private Int32 top = 20;
+        public Int32 Top { set { top = value; } get { return top; } }
+        public Int32 Left { set { left = value; } get { return left; } }
+        private DataTable printedTable;
+        private Int32 pheight;
+        private Int32 pWidth;
+        private Int32 pindex;
+        private Int32 curdgi;
+        private Int32[] width;
+        private Int32 rowOfDownDistance = 3;
+        private Int32 rowOfUpDistance = 2;
+
+        Int32 startColumnControls = 0;
+        Int32 endColumnControls = 0;
+
+        #endregion
+
+        #region Method for PrintDataTable //打印数据集
+        /// <summary>
+        /// 打印数据集
+        /// </summary>
+        /// <param name="table">数据集</param>
+        /// <returns></returns>
+        public Boolean PrintDataTable(DataTable table)
+        {
+            PrintDocument prtDocument = new PrintDocument();
+            try
+            {
+                if (printName != String.Empty)
+                {
+                    prtDocument.PrinterSettings.PrinterName = printName;
+                }
+                //prtDocument.DefaultPageSettings.Landscape = true;//设置打印方向为横向
+                prtDocument.OriginAtMargins = false;
+                PrintDialog prtDialog = new PrintDialog();
+                prtDialog.AllowSomePages = true;
+                prtDialog.ShowHelp = false;
+                prtDialog.Document = prtDocument;
+                if (DialogResult.OK != prtDialog.ShowDialog())
+                {
+                    return false;
+                }
+                printedTable = table;
+                pindex = 0;
+                curdgi = 0;
+                width = new Int32[printedTable.Columns.Count];
+                pheight = prtDocument.PrinterSettings.DefaultPageSettings.Bounds.Bottom + 400;
+                //pheight = prtDocument.PrinterSettings.DefaultPageSettings.Bounds.Bottom;
+                pWidth = prtDocument.PrinterSettings.DefaultPageSettings.Bounds.Right;
+                prtDocument.PrintPage += new PrintPageEventHandler(docToPrint_PrintPage);
+                prtDocument.Print();
+            }
+            catch (InvalidPrinterException ex)
+            {
+                MessageBox.Show("没有安装打印机");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            prtDocument.Dispose();
+            return true;
+        }
+        #endregion
+
+        #region Handler for docToPrint_PrintPage //设置打印机开始打印的事件处理函数
+        /// <summary>
+        /// 设置打印机开始打印的事件处理函数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="ev"></param>
+        private void docToPrint_PrintPage(object sender,
+         System.Drawing.Printing.PrintPageEventArgs ev)//设置打印机开始打印的事件处理函数
+        {
+            Int32 x = left;
+            Int32 y = top;
+            Int32 rowOfTop = top;
+            Int32 i;
+            Pen pen = new Pen(Brushes.Black, 1);
+            if (0 == pindex)
+            {
+                for (i = 0; i < titles.Length; i++)
+                {
+                    ev.Graphics.DrawString(titles[i], prtTitleFont, Brushes.Black, left, y + rowOfUpDistance);
+                    y += prtTextFont.Height + rowOfDownDistance;
+                }
+                rowOfTop = y;
+                foreach (System.Data.DataRow dr in printedTable.Rows)
+                {
+                    for (i = 0; i < printedTable.Columns.Count; i++)
+                    {
+                        Int32 colwidth = Convert.ToInt16(ev.Graphics.MeasureString(dr[i].ToString().Trim(), prtTextFont).Width);
+                        if (colwidth > width[i])
+                        {
+                            width[i] = colwidth;
+                        }
+                    }
+                }
+            }
+            for (i = endColumnControls; i < printedTable.Columns.Count; i++)
+            {
+                String headtext = printedTable.Columns[i].ColumnName.Trim();
+                if (pindex == 0)
+                {
+                    int colwidth = Convert.ToInt16(ev.Graphics.MeasureString(headtext, prtTextFont).Width);
+                    if (colwidth > width[i])
+                    {
+                        width[i] = colwidth;
+                    }
+                }
+                //判断宽是否越界
+                if (x + width[i] > pheight)
+                {
+                    break;
+                }
+                ev.Graphics.DrawString(headtext, prtTextFont, Brushes.Black, x, y + rowOfUpDistance);
+                x += width[i];
+            }
+            startColumnControls = endColumnControls;
+            if (i < printedTable.Columns.Count)
+            {
+                endColumnControls = i;
+                ev.HasMorePages = true;
+            }
+            else
+            {
+                endColumnControls = printedTable.Columns.Count;
+            }
+            ev.Graphics.DrawLine(pen, left, rowOfTop, x, rowOfTop);
+            y += rowOfUpDistance + prtTextFont.Height + rowOfDownDistance;
+            ev.Graphics.DrawLine(pen, left, y, x, y);
+            //打印数据
+            for (i = curdgi; i < printedTable.Rows.Count; i++)
+            {
+                x = left;
+                for (Int32 j = startColumnControls; j < endColumnControls; j++)
+                {
+                    ev.Graphics.DrawString(printedTable.Rows[i][j].ToString().Trim(), prtTextFont, Brushes.Black, x, y + rowOfUpDistance);
+                    x += width[j];
+                }
+                y += rowOfUpDistance + prtTextFont.Height + rowOfDownDistance;
+                ev.Graphics.DrawLine(pen, left, y, x, y);
+                //判断高是否越界
+                if (y > pWidth - prtTextFont.Height - 400) //if (y > pWidth - prtTextFont.Height - 30)
+                {
+                    break;
+                }
+            }
+            ev.Graphics.DrawLine(pen, left, rowOfTop, left, y);
+            x = left;
+            for (Int32 k = startColumnControls; k < endColumnControls; k++)
+            {
+                x += width[k];
+                ev.Graphics.DrawLine(pen, x, rowOfTop, x, y);
+            }
+            //判断高是否越界
+            if (y > pWidth - prtTextFont.Height - 400) //if (y > pWidth - prtTextFont.Height - 30) 
+            {
+                pindex++; if (0 == startColumnControls)
+                {
+                    curdgi = i + 1;
+                }
+                if (!ev.HasMorePages)
+                {
+                    endColumnControls = 0;
+                }
+                ev.HasMorePages = true;
+            }
+        }
+        #endregion
+    }
+}
---
+++ SVN_非标接口/Comm/SQLHelper.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
+using System.Xml;
+using System.Text;
+
+
+namespace HealthExaminationSystem.ThirdParty.DataTransmission
+{
+    public static class SQLHelper
+    {
+
+        public static string SelectSQl(string strsql, string strcon)
+        {
+            string strres = "";
+            using (SqlConnection con = new SqlConnection(strcon))
+            {
+                SqlCommand cmd = new SqlCommand(strsql, con);
+                con.Open();
+                try
+                {
+                    strres = cmd.ExecuteScalar().ToString();
+                }
+                catch (Exception)
+                {
+
+
+                }
+
+                WriteTxt(strsql);
+                con.Dispose();
+                con.Close();
+            }
+            return strres;
+
+        }
+        public static DataTable SelectData(string strsql, string strcon)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(strcon))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(strsql, con);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+
+                con.Dispose();
+                con.Close();
+                //WriteTxt(strsql);
+            }
+            return dt;
+
+        }
+        public static int UpdateSQl(string strsql, string strcon)
+        {
+            int ires = 0;
+            using (SqlConnection con = new SqlConnection(strcon))
+            {
+                SqlCommand cmd = new SqlCommand(strsql, con);
+                con.Open();
+                ires = cmd.ExecuteNonQuery();
+                WriteTxt(strsql);
+                con.Dispose();
+                con.Close();
+            }
+            return ires;
+
+        }
+      
+       
+        public static bool WriteTxt(string str)
+        {
+            try
+            {
+                string path = System.Environment.CurrentDirectory + "\\log";
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                FileStream fs = new FileStream(System.Environment.CurrentDirectory + "\\log\\Log.txt", FileMode.Append);
+                StreamWriter sw = new StreamWriter(fs);
+                //开始写入
+                sw.WriteLine(str);
+                //清空缓冲区
+                sw.Flush();
+                //关闭流
+                sw.Close();
+                fs.Close();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool AddSystemLog(string EmployeeId, string Object, string LogText, string strjcon)
+        {
+            LogText = LogText.Replace("'", "$");
+            string strsql = "insert into systemlog(LogTime,EmployeeId,IP" +
+                    ",Object,LogText) values(getdate(),'" + EmployeeId + "','192.168.0.1','" + Object + "','" + LogText + "')";
+            UpdateSQl(strsql, strjcon);
+
+            return true;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dtitem">需要转换的数据</param>
+        /// <param name="DataTableName">转入的表名</param>
+        /// <param name="strcolumnName">暂时没有</param>
+        /// <param name="progressBarControl1">进度条</param>
+        public static void InsertCusData(DataTable dtitem, string DataTableName, string strCusName, string strcon)
+        {
+            //SQLHelpe.v9_ExcuteNonQuery(dtitem, DataTableName);
+            //return;
+            StringBuilder strinsertsql = new StringBuilder();
+            strinsertsql.Append(" insert into QZJ." + DataTableName + " (");
+            for (int d = 0; d < dtitem.Columns.Count; d++)
+            {
+                if (dtitem.Columns[d].ToString() != "UPLOADDATETIME")
+                {
+                    strinsertsql.Append(dtitem.Columns[d].ToString() + ",");
+                }
+
+            }
+            int inum = 1;
+            string strsql = "";
+            StringBuilder strsuminsertsql = new StringBuilder();
+            int isumnum = 1;
+
+            foreach (DataRow item in dtitem.Rows)
+            {
+                try
+                {
+                    StringBuilder strb2 = new StringBuilder();
+                    strb2.Append(strinsertsql.ToString().TrimEnd(',') + ") VALUES (");
+                    for (int d = 0; d < dtitem.Columns.Count; d++)
+                    {
+                        if (dtitem.Columns[d].ToString() == "UPLOADDATETIME")
+                        {
+                            continue;
+                        }
+                        if (dtitem.Columns[d].ToString() == "BATCHID")
+                        {
+                            strb2.Append("'" + strCusName + "',");
+                        }
+                        else
+                        {
+                            strb2.Append("'" + item[dtitem.Columns[d].ToString()].ToString() + "',");
+                        }
+
+                    }
+                    strsql = strb2.ToString().TrimEnd(',') + ")";
+                    strsuminsertsql.Append(strsql + " ");
+                    if (isumnum == 1000 || inum == dtitem.Rows.Count)
+                    {
+                        isumnum = 1;
+                        //System.Threading.Thread.Sleep(10);
+                       
+                        strsuminsertsql.Clear();
+                        //WriteTxt("转换:" + DataTableName
+                        //    + "表\t;" + item[strcolumnName].ToString() +
+                        //    "\t\t数据成功\t;总计:" + dtitem.Rows.Count + "条;当前记录:" + inum + "条; \n\r", mtxtiteminfo);
+                    }
+
+
+                    inum++;
+                    isumnum++;
+                }
+                catch (Exception ex)
+                {
+                    WriteTxt("转换失败:" + DataTableName + "\n\r 错误原因:" + ex.Message + "\n\r SQL:" + strsuminsertsql.ToString());
+                    //SQLHelpe.WriteData("转换" + DataTableName + "表数据时出现错误：" + ex.Message + "; SQL:" + strsql.ToString() + "\n\r");
+                }
+            }
+        }
+    }
+}
